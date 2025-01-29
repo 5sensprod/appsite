7b1726004995af38d1f0ff3ca40166e4c425fc4a
chris <5sensprod@gmail.com>
2025-01-29 19:09:00 +0100

    fix avertissement mjs

--- electron/main.js
+++ electron/main.js
@@ -1,5 +1,9 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -13,9 +17,7 @@ function createWindow() {
   });
 
   if (process.env.NODE_ENV === 'development') {
-    // Changer cette ligne
     win.loadURL('http://localhost:5173');
-    win.webContents.openDevTools();
   } else {
     win.loadFile(path.join(__dirname, '../dist/index.html'));
   }
--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
       <div className="rounded-lg bg-white p-8 shadow-lg">
-        <h1 className="text-2xl font-bold text-gray-900">AppSite</h1>
+        <h1 className="text-2xl font-bold text-gray-900">ViteElectronTailwind</h1>
       </div>
     </div>
   );
